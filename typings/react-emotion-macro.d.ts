cb9d783484e378f1e5c4ec3acfeaea42bfc9c4a9
Mathieu Dumont <dumont.m@icloud.com>
2018-11-07 12:19:39 -0500

    Fix react-emotion/macro typings

--- typings/react-emotion-macro.d.ts
+++ typings/react-emotion-macro.d.ts
@@ -1,4 +1,4 @@
 declare module 'react-emotion/macro' {
-  export * from 'react-emotion/macro';
-  export {default} from 'react-emotion/macro';
+  export * from 'react-emotion';
+  export {default} from 'react-emotion';
 }
